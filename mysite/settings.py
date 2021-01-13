7bb67957594c73de107094d1194736d47535a94b
ikromnurrohim <ikromnurrohim16@gmail.com>
2021-01-13 23:42:14 +0700

    search

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROO = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
